83bd95b963d8da78d34596d510e821d709081286
Mark Percival <m@mdp.im>
2020-07-03 14:08:34 -0400

    WIP: Better output formatting

--- src/FormulatedAutomation/Profiler/system_base.py
+++ src/FormulatedAutomation/Profiler/system_base.py
@@ -3,12 +3,20 @@ System profiling base module abstraction
 """
 import re
 import os
+import sys
 import datetime
 import platform
 import yaml
 import pkg_resources
 from robot.libraries.BuiltIn import BuiltIn
 from .utils import Utils
+
+DEFAULT_PROFILE_KEY_ORDER = [
+    'metadata',
+    'system',
+    'robot_framework',
+    'python',
+]
 
 class SystemBase:
     """ Our base class for all system profile classes.
@@ -32,6 +40,7 @@ class SystemBase:
             'system': platform.system(),
             'platform': platform.platform(),
             'processor': platform.processor(),
+            'environment_variables': self.system_environment_variables(),
         }
         try:
             # Will fail in restricted shells
@@ -43,6 +52,20 @@ class SystemBase:
 
     def system_environment_variables(self):
         return Utils.dump_collection(os.environ)
+
+    def python(self):
+        """ List out all python information
+        """
+        return {
+            'version': sys.version,
+            'executable': sys.executable,
+            'exec_prefix': sys.exec_prefix,
+            'packages': self.python_packages(),
+        }
+        installed_packages = pkg_resources.working_set
+        installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
+                                          for i in installed_packages])
+        return installed_packages_list
 
     def python_packages(self):
         """ List out all the installed python packages and versions
@@ -64,33 +87,44 @@ class SystemBase:
                 'run_at': datetime.datetime.utcnow(),
                 'profiler': self.__class__.__name__,
             },
-            'variables': variables,
+            'robot_framework': {
+                'variables': variables,
+            },
             'system': self.system_info(),
-            'environment_variables': self.system_environment_variables(),
-            'python_packages': self.python_packages(),
+            'python': self.python(),
             'programs': self.get_programs(),
         }
         # TODO: @mdp allow for sending as JSON to a remote endpoint
-        self._write_orderly_yaml(profile, output_file)
+        self.__write_orderly_yaml(profile, output_file)
 
-    def _write_orderly_yaml(self, profile, outfile):
+    def __profile_key_order(self):
+        return DEFAULT_PROFILE_KEY_ORDER
+
+    def __sorted_profile(self, profile):
+        """ Returns a multi-dimensional array of values
+        eg. [[key, dict], [key, dict]]
+        for the purpose of dumping to an specifically ordered yaml file
+        """
+
+        key_order = self.__profile_key_order()
+        profile_arr = []
+
+        for key in key_order:
+            val = profile.pop(key, {})
+            profile_arr.append([key, val])
+
+        for key, val in profile.items():
+            profile_arr.append([key, val])
+
+        return profile_arr
+
+    def __write_orderly_yaml(self, profile, outfile):
         """ Yaml autosorts keys alphabetically, but we don't want this at the
         top level, we want to be able to put the most important information
         first.
         """
-
+        profile_arr = self.__sorted_profile(profile)
         with open(outfile, 'w+') as f:
-            yaml.dump({'metadata': profile['metadata']},
-                      f, default_flow_style=False)
-            yaml.dump({'system': profile['system']},
-                      f, default_flow_style=False)
-            yaml.dump({'variables': profile['variables']},
-                      f, default_flow_style=False)
-            yaml.dump(
-                {'environment_variables': profile['environment_variables']},
-                f, default_flow_style=False)
-            yaml.dump(
-                {'python_packages': profile['python_packages']},
-                f, default_flow_style=False)
-            yaml.dump({'programs': profile['programs']},
-                      f, default_flow_style=False)
+            for item in profile_arr:
+                yaml.dump({item[0]: item[1]},
+                          f, default_flow_style=False)
